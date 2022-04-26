c5d1a62e3132e70e6c7a2ed105100ecd3c40077e
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-04-26 14:04:32 +0200

    Initial Commit + Deviant Art Page Class Setup

---
+++ src/test/java/DeviantArtPageBase.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class DeviantArtPageBase {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    public MicrosoftPageBase(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    protected WebElement waitAndReturnElement(By locator) {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this.driver.findElement(locator);
+    }
+
+    public String getBodyText() {
+        WebElement bodyElement = this.waitAndReturnElement(By.tagName("body"));
+        return bodyElement.getText();
+    }
+
+}
